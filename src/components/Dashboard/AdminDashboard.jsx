68568f9cea7599247be9d91425a90aa5d5c96ca3
delta student <sunilmaurya10864@gmail.com>
2026-07-15 21:41:50 +0530

    changes

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../other/Header'
-import CreateTask from '../other/CreateTask'
-import AllTask from '../other/AllTask'
+import Header from '../Other/Header'
+import CreateTask from '../Other/CreateTask'
+import AllTask from '../Other/AllTask'
 
 const AdminDashboard = (props) => {
     return (
