abe54e4573e5cb70b45171bfddf2f348eba4a9ea
delta student <sunilmaurya10864@gmail.com>
2026-07-15 21:44:33 +0530

    chnages

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../other/Header'
-import TaskListNumbers from '../other/TaskListNumbers'
+import Header from '../Other/Header'
+import TaskListNumbers from '../Other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = (props) => {
